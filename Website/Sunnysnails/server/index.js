5c0c85a2965f3362780eaafd1fc30c2db5adc5fb
Michael Lee <mlee@vellichorlabs.com>
2017-04-09 15:03:10 -0500

    Added components to project

--- Website/Sunnysnails/server/index.js
+++ Website/Sunnysnails/server/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = require('./app');
+const app = require('./server');
 
 const PORT = process.env.PORT || 8080;
 
--- Website/Sunnysnails/src/app/app.module.ts
+++ Website/Sunnysnails/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SignupComponent } from './signup/signup.component';
+import { WhoweareComponent } from './whoweare/whoweare.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent,
+    SignupComponent,
+    WhoweareComponent,
+    ContactusComponent,
+    GalleryComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
