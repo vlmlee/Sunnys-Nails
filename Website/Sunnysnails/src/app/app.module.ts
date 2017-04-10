6010d3b6a34fa493cfaa12eeda2639968e88d07f
Michael Lee <mlee@vellichorlabs.com>
2017-04-10 01:23:05 -0500

    Added views and styles.

--- Website/Sunnysnails/src/app/app.module.ts
+++ Website/Sunnysnails/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
-import { LandingComponent } from './landing/landing.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
-import { SignupComponent } from './signup/signup.component';
-import { WhoweareComponent } from './whoweare/whoweare.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './views/landing/hero/hero.component';
+import { LandingComponent } from './views/landing/landing.component';
+import { AboutComponent } from './views/about/about.component';
+import { ServicesComponent } from './views/services/services.component';
+import { SignupComponent } from './views/signup/signup.component';
+import { WhoweareComponent } from './views/whoweare/whoweare.component';
+import { ContactusComponent } from './views/contactus/contactus.component';
+import { GalleryComponent } from './views/gallery/gallery.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -26,15 +29,18 @@ import { NavigationComponent } from './navigation/navigation.component';
     WhoweareComponent,
     ContactusComponent,
     GalleryComponent,
-    NavigationComponent
+    NavigationComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     HttpModule,
-    Ng2PageScrollModule.forRoot()
+    Ng2PageScrollModule.forRoot(),
+    CarouselModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ Website/Sunnysnails/src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PageScrollConfig } from 'ng2-page-scroll';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() {
+    PageScrollConfig.defaultScrollOffset = 50;
+    PageScrollConfig.defaultEasingLogic = {
+        ease: (t: number, b: number, c: number, d: number): number => {
+            // easeInOutExpo easing
+            if (t === 0) return b;
+            if (t === d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    };
+    PageScrollConfig.defaultInterruptible = false;
+   }
+
+  ngOnInit() {
+  }
+
+}
