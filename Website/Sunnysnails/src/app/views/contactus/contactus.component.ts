02895d039df219779e7043efab22a196984cc0da
Michael Lee <mlee@vellichorlabs.com>
2017-05-08 23:44:54 -0500

    Updated.

--- Website/Sunnysnails/src/app/views/contactus/contactus.component.ts
+++ Website/Sunnysnails/src/app/views/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.initMap();
+    }, 1000);
+  }
+
+  initMap() {
+    var nails = { lat: 42.1091478, lng: -87.9674743 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: nails,
+      scrollwheel: false,
+      navigationControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      draggable: true
+    });
+    var marker = new google.maps.Marker({
+      position: nails,
+      map: map
+    });
+  }
 }
